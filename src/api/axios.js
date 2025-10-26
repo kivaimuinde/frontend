5df5ae6948eaaca9899b55cd3d9e460c87b45a85
kivaimuinde <muindejosphat@gmail.com>
2025-10-26 19:23:25 +0300

    update baseURL for hosted backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL:"https://muindejk.pythonanywhere.com/api",
+  // baseURL: "http://127.0.0.1:8000/api/",
 });
 
 api.interceptors.request.use((config) => {
